285ef01b9822382d9730e9a4ab2da719ba9e68c6
park minho <>
2021-09-26 11:10:41 +0900

    #4 paragraph tag change div to p

--- example/blockeditor.js
+++ example/blockeditor.js
@@ -1,5 +1,5 @@
 let currentBlock;
-
+const paragraph_element = "p";
 function getSvgDefines() {
 const svgDefines = `
 	<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
@@ -35,6 +35,87 @@ const svgDefines = `
 		</symbol>
 		<symbol id="nav_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
 			<polygon points="9,13.5 14.7,7.9 13.2,6.5 9,10.7 4.8,6.5 3.3,7.9 "></polygon>
+		</symbol>
+		<symbol id="edit_pencil" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M20.1 5.1L16.9 2 6.2 12.7l-1.3 4.4 4.5-1.3L20.1 5.1zM4 20.8h8v-1.5H4v1.5z"></path>		
+		</symbol>
+		<symbol id="edit_undo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M18.3 11.7c-.6-.6-1.4-.9-2.3-.9H6.7l2.9-3.3-1.1-1-4.5 5L8.5 16l1-1-2.7-2.7H16c.5 0 .9.2 1.3.5 1 1 1 3.4 1 4.5v.3h1.5v-.2c0-1.5 0-4.3-1.5-5.7z"></path>
+		</symbol>
+		<symbol id="edit_redo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M15.6 6.5l-1.1 1 2.9 3.3H8c-.9 0-1.7.3-2.3.9-1.4 1.5-1.4 4.2-1.4 5.6v.2h1.5v-.3c0-1.1 0-3.5 1-4.5.3-.3.7-.5 1.3-.5h9.2L14.5 15l1.1 1.1 4.6-4.6-4.6-5z"></path>
+		</symbol>
+		<symbol id="infomation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M12 3.2c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8 0-4.8-4-8.8-8.8-8.8zm0 16c-4 0-7.2-3.3-7.2-7.2C4.8 8 8 4.8 12 4.8s7.2 3.3 7.2 7.2c0 4-3.2 7.2-7.2 7.2zM11 17h2v-6h-2v6zm0-8h2V7h-2v2z"></path>
+		</symbol>
+		<symbol id="heading" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M6.2 5.2v13.4l5.8-4.8 5.8 4.8V5.2z"></path>
+		</symbol>
+		<symbol id="listmode" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M4 4v1.5h16V4H4zm8 8.5h8V11h-8v1.5zM4 20h16v-1.5H4V20zm4-8c0-1.1-.9-2-2-2s-2 .9-2 2 .9 2 2 2 2-.9 2-2z"></path>
+		</symbol>
+		<symbol id="bullet" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M11.1 15.8H20v-1.5h-8.9v1.5zm0-8.6v1.5H20V7.2h-8.9zM6 13c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-7c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
+		</symbol>
+		<symbol id="numbering" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<path d="M11.1 15.8H20v-1.5h-8.9v1.5zm0-8.6v1.5H20V7.2h-8.9zM5 6.7V10h1V5.3L3.8 6l.4 1 .8-.3zm-.4 5.7c-.3.1-.5.2-.7.3l.1 1.1c.2-.2.5-.4.8-.5.3-.1.6 0 .7.1.2.3 0 .8-.2 1.1-.5.8-.9 1.6-1.4 2.5h2.7v-1h-1c.3-.6.8-1.4.9-2.1.1-.3 0-.8-.2-1.1-.5-.6-1.3-.5-1.7-.4z"></path>
+		</symbol>
+		<symbol id="h1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<path d="M9 5h2v10H9v-4H5v4H3V5h2v4h4V5zm6.6 0c-.6.9-1.5 1.7-2.6 2v1h2v7h2V5h-1.4z"></path>
+		</symbol>
+		<symbol id="h2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<path d="M7 5h2v10H7v-4H3v4H1V5h2v4h4V5zm8 8c.5-.4.6-.6 1.1-1.1.4-.4.8-.8 1.2-1.3.3-.4.6-.8.9-1.3.2-.4.3-.8.3-1.3 0-.4-.1-.9-.3-1.3-.2-.4-.4-.7-.8-1-.3-.3-.7-.5-1.2-.6-.5-.2-1-.2-1.5-.2-.4 0-.7 0-1.1.1-.3.1-.7.2-1 .3-.3.1-.6.3-.9.5-.3.2-.6.4-.8.7l1.2 1.2c.3-.3.6-.5 1-.7.4-.2.7-.3 1.2-.3s.9.1 1.3.4c.3.3.5.7.5 1.1 0 .4-.1.8-.4 1.1-.3.5-.6.9-1 1.2-.4.4-1 .9-1.6 1.4-.6.5-1.4 1.1-2.2 1.6V15h8v-2H15z"></path>
+		</symbol>
+		<symbol id="h3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<path d="M12.1 12.2c.4.3.8.5 1.2.7.4.2.9.3 1.4.3.5 0 1-.1 1.4-.3.3-.1.5-.5.5-.8 0-.2 0-.4-.1-.6-.1-.2-.3-.3-.5-.4-.3-.1-.7-.2-1-.3-.5-.1-1-.1-1.5-.1V9.1c.7.1 1.5-.1 2.2-.4.4-.2.6-.5.6-.9 0-.3-.1-.6-.4-.8-.3-.2-.7-.3-1.1-.3-.4 0-.8.1-1.1.3-.4.2-.7.4-1.1.6l-1.2-1.4c.5-.4 1.1-.7 1.6-.9.5-.2 1.2-.3 1.8-.3.5 0 1 .1 1.6.2.4.1.8.3 1.2.5.3.2.6.5.8.8.2.3.3.7.3 1.1 0 .5-.2.9-.5 1.3-.4.4-.9.7-1.5.9v.1c.6.1 1.2.4 1.6.8.4.4.7.9.7 1.5 0 .4-.1.8-.3 1.2-.2.4-.5.7-.9.9-.4.3-.9.4-1.3.5-.5.1-1 .2-1.6.2-.8 0-1.6-.1-2.3-.4-.6-.2-1.1-.6-1.6-1l1.1-1.4zM7 9H3V5H1v10h2v-4h4v4h2V5H7v4z"></path>
+		</symbol>
+		<symbol id="h4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<path d="M9 15H7v-4H3v4H1V5h2v4h4V5h2v10zm10-2h-1v2h-2v-2h-5v-2l4-6h3v6h1v2zm-3-2V7l-2.8 4H16z"></path>
+		</symbol>
+		<symbol id="h5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<path d="M12.1 12.2c.4.3.7.5 1.1.7.4.2.9.3 1.3.3.5 0 1-.1 1.4-.4.4-.3.6-.7.6-1.1 0-.4-.2-.9-.6-1.1-.4-.3-.9-.4-1.4-.4H14c-.1 0-.3 0-.4.1l-.4.1-.5.2-1-.6.3-5h6.4v1.9h-4.3L14 8.8c.2-.1.5-.1.7-.2.2 0 .5-.1.7-.1.5 0 .9.1 1.4.2.4.1.8.3 1.1.6.3.2.6.6.8.9.2.4.3.9.3 1.4 0 .5-.1 1-.3 1.4-.2.4-.5.8-.9 1.1-.4.3-.8.5-1.3.7-.5.2-1 .3-1.5.3-.8 0-1.6-.1-2.3-.4-.6-.2-1.1-.6-1.6-1-.1-.1 1-1.5 1-1.5zM9 15H7v-4H3v4H1V5h2v4h4V5h2v10z"></path>
+		</symbol>
+		<symbol id="h6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<path d="M9 15H7v-4H3v4H1V5h2v4h4V5h2v10zm8.6-7.5c-.2-.2-.5-.4-.8-.5-.6-.2-1.3-.2-1.9 0-.3.1-.6.3-.8.5l-.6.9c-.2.5-.2.9-.2 1.4.4-.3.8-.6 1.2-.8.4-.2.8-.3 1.3-.3.4 0 .8 0 1.2.2.4.1.7.3 1 .6.3.3.5.6.7.9.2.4.3.8.3 1.3s-.1.9-.3 1.4c-.2.4-.5.7-.8 1-.4.3-.8.5-1.2.6-1 .3-2 .3-3 0-.5-.2-1-.5-1.4-.9-.4-.4-.8-.9-1-1.5-.2-.6-.3-1.3-.3-2.1s.1-1.6.4-2.3c.2-.6.6-1.2 1-1.6.4-.4.9-.7 1.4-.9.6-.3 1.1-.4 1.7-.4.7 0 1.4.1 2 .3.5.2 1 .5 1.4.8 0 .1-1.3 1.4-1.3 1.4zm-2.4 5.8c.2 0 .4 0 .6-.1.2 0 .4-.1.5-.2.1-.1.3-.3.4-.5.1-.2.1-.5.1-.7 0-.4-.1-.8-.4-1.1-.3-.2-.7-.3-1.1-.3-.3 0-.7.1-1 .2-.4.2-.7.4-1 .7 0 .3.1.7.3 1 .1.2.3.4.4.6.2.1.3.3.5.3.2.1.5.2.7.1z"></path>
+		</symbol>
+		<symbol id="table" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM5 4.5h14c.3 0 .5.2.5.5v3.5h-15V5c0-.3.2-.5.5-.5zm8 5.5h6.5v3.5H13V10zm-1.5 3.5h-7V10h7v3.5zm-7 5.5v-4h7v4.5H5c-.3 0-.5-.2-.5-.5zm14.5.5h-6V15h6.5v4c0 .3-.2.5-.5.5z"></path>
+		</symbol>
+		<symbol id="image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM5 4.5h14c.3 0 .5.2.5.5v8.4l-3-2.9c-.3-.3-.8-.3-1 0L11.9 14 9 12c-.3-.2-.6-.2-.8 0l-3.6 2.6V5c-.1-.3.1-.5.4-.5zm14 15H5c-.3 0-.5-.2-.5-.5v-2.4l4.1-3 3 1.9c.3.2.7.2.9-.1L16 12l3.5 3.4V19c0 .3-.2.5-.5.5z"></path>
+		</symbol>
+		<symbol id="tag" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M20.1 11.2l-6.7-6.7c-.1-.1-.3-.2-.5-.2H5c-.4-.1-.8.3-.8.7v7.8c0 .2.1.4.2.5l6.7 6.7c.2.2.5.4.7.5s.6.2.9.2c.3 0 .6-.1.9-.2.3-.1.5-.3.8-.5l5.6-5.6c.4-.4.7-1 .7-1.6.1-.6-.2-1.2-.6-1.6zM19 13.4L13.4 19c-.1.1-.2.1-.3.2-.2.1-.4.1-.6 0-.1 0-.2-.1-.3-.2l-6.5-6.5V5.8h6.8l6.5 6.5c.2.2.2.4.2.6 0 .1 0 .3-.2.5zM9 8c-.6 0-1 .4-1 1s.4 1 1 1 1-.4 1-1-.4-1-1-1z"></path>
+		</symbol>
+		<symbol id="imagetext" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M4 17h7V6H4v11zm9-10v1.5h7V7h-7zm0 5.5h7V11h-7v1.5zm0 4h7V15h-7v1.5z"></path>
+		</symbol>
+		<symbol id="imageleft" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M4 18h6V6H4v12zm9-10v1.5h7V8h-7zm0 7.5h7V14h-7v1.5z"></path>
+		</symbol>
+		<symbol id="imageright" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M14 6v12h6V6h-6zM4 9.5h7V8H4v1.5zm0 6h7V14H4v1.5z"></path>
+		</symbol>
+		<symbol id="code" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M20.8 10.7l-4.3-4.3-1.1 1.1 4.3 4.3c.1.1.1.3 0 .4l-4.3 4.3 1.1 1.1 4.3-4.3c.7-.8.7-1.9 0-2.6zM4.2 11.8l4.3-4.3-1-1-4.3 4.3c-.7.7-.7 1.8 0 2.5l4.3 4.3 1.1-1.1-4.3-4.3c-.2-.1-.2-.3-.1-.4z"></path>
+		</symbol>
+		<symbol id="separate" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M20.2 7v4H3.8V7H2.2v9h1.6v-3.5h16.4V16h1.6V7z"></path>
+		</symbol>
+		<symbol id="gallery" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M20.2 8v11c0 .7-.6 1.2-1.2 1.2H6v1.5h13c1.5 0 2.7-1.2 2.7-2.8V8h-1.5zM18 16.4V4.6c0-.9-.7-1.6-1.6-1.6H4.6C3.7 3 3 3.7 3 4.6v11.8c0 .9.7 1.6 1.6 1.6h11.8c.9 0 1.6-.7 1.6-1.6zM4.5 4.6c0-.1.1-.1.1-.1h11.8c.1 0 .1.1.1.1V12l-2.3-1.7c-.3-.2-.6-.2-.9 0l-2.9 2.1L8 11.3c-.2-.1-.5-.1-.7 0l-2.9 1.5V4.6zm0 11.8v-1.8l3.2-1.7 2.4 1.2c.2.1.5.1.8-.1l2.8-2 2.8 2v2.5c0 .1-.1.1-.1.1H4.6c0-.1-.1-.2-.1-.2z"></path>
+		</symbol>
+		<symbol id="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M19 6.5H5c-1.1 0-2 .9-2 2v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7c0-1.1-.9-2-2-2zm.5 9c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5v-7c0-.3.2-.5.5-.5h14c.3 0 .5.2.5.5v7zM8 13h8v-1.5H8V13z"></path>
+		</symbol>
+		<symbol id="archive" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M19 6.2h-5.9l-.6-1.1c-.3-.7-1-1.1-1.8-1.1H5c-1.1 0-2 .9-2 2v11.8c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8.2c0-1.1-.9-2-2-2zm.5 11.6c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h5.8c.2 0 .4.1.4.3l1 2H19c.3 0 .5.2.5.5v9.5zM8 12.8h8v-1.5H8v1.5zm0 3h8v-1.5H8v1.5z"></path>
+		</symbol>
+		<symbol id="quote" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M13 6v6h5.2v4c0 .8-.2 1.4-.5 1.7-.6.6-1.6.6-2.5.5h-.3v1.5h.5c1 0 2.3-.1 3.3-1 .6-.6 1-1.6 1-2.8V6H13zm-9 6h5.2v4c0 .8-.2 1.4-.5 1.7-.6.6-1.6.6-2.5.5h-.3v1.5h.5c1 0 2.3-.1 3.3-1 .6-.6 1-1.6 1-2.8V6H4v6z"></path>
+		</symbol>
+		<symbol id="link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path d="M15.6 7.2H14v1.5h1.6c2 0 3.7 1.7 3.7 3.7s-1.7 3.7-3.7 3.7H14v1.5h1.6c2.8 0 5.2-2.3 5.2-5.2 0-2.9-2.3-5.2-5.2-5.2zM4.7 12.4c0-2 1.7-3.7 3.7-3.7H10V7.2H8.4c-2.9 0-5.2 2.3-5.2 5.2 0 2.9 2.3 5.2 5.2 5.2H10v-1.5H8.4c-2 0-3.7-1.7-3.7-3.7zm4.6.9h5.3v-1.5H9.3v1.5z"></path>
 		</symbol>
 	</svg>	
 `;
@@ -82,7 +163,7 @@ function focusBlockItem(e) {
 
 function appendSection(targetEl, isChild) {
 	var newBlock = document.createElement('section');
-	var div = document.createElement('div');
+	var div = document.createElement(paragraph_element);
 	var spanEl = document.createElement('span');
 	spanEl.setAttribute('data-rich-text-placeholder','글쓰기로 시작하거나 / 키를 눌러 블럭 선택');
 	spanEl.setAttribute('contenteditable', 'false');
@@ -120,7 +201,7 @@ class Editor {
 	}
 
 	initEditor() {
-		document.execCommand("defaultParagraphSeparator", false, "div");
+		document.execCommand("defaultParagraphSeparator", false, paragraph_element);
 	}
 
 	bindEvent() {
@@ -289,18 +370,19 @@ class BlockEditor {
 	getHTML() {
 		let html = "";
 		$('#editor-contents').children().each(function(index, sectionEl) {
-			html += "<div class=\"section\">";
-			console.log(index + ": " + $(sectionEl).html());
-			$(sectionEl).each(function(indexChild, childEl) {
-				console.log(childEl.tagName);
-				//if(childEl.tagName=="span" && childEl.hasAttribute("data-rich-text-placeholder")) {
-				html += $(childEl).html();
-			//	console.log(index2 + ": " + $(item).html());
-			});
-			html += "</div>";
+			if(!isEmptyEditBlock(sectionEl)) {
+				html += "<section>";
+				console.log(index + ": " + $(sectionEl).html());
+				$(sectionEl).each(function(indexChild, childEl) {
+					console.log(childEl.tagName);
+					//if(childEl.tagName=="span" && childEl.hasAttribute("data-rich-text-placeholder")) {
+					html += $(childEl).html().replaceAll('<span data-rich-text-placeholder="글쓰기로 시작하거나 / 키를 눌러 블럭 선택" contenteditable="false" class="span-placeholder"></span>','');
+				});
+				html += "</section>";
+			}
 		});
-		//return html;
-		return $('#editor-contents').html();
+		return html;
+		//return $('#editor-contents').html();
 	}
 
 	bindEvent() {
